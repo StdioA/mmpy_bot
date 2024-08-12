8e2da1737172789dc6209de501f9625443a13b7a
David Dai <stdioa@163.com>
2024-08-12 17:42:59 +0800

    Increase the poll interval to reduce CPU consumption

--- mmpy_bot/event_handler.py
+++ mmpy_bot/event_handler.py
@@ -50,8 +50,7 @@ class EventHandler(object):
                 event = webhook_queue.get_nowait()
                 await self._handle_webhook(event)
             except queue.Empty:
-                pass
-            await asyncio.sleep(0.0001)
+                await asyncio.sleep(0.5)
 
     async def _handle_event(self, data):
         post = json.loads(data)
